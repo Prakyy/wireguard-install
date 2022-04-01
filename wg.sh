c7d11381f0d0917a1b7d6818b8f4f334094dd8fd
Pr_k_rsh <43814894+Prakyy@users.noreply.github.com>
2022-04-01 20:42:04 +0530

    Friendlier

--- wg.sh
+++ wg.sh
@@ -9,7 +9,7 @@ NC='\033[0m'
 
 function isRoot() {
 	if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
+		echo "This script needs root permission. Prefix the run command with sudo..."
 		exit 1
 	fi
 }
@@ -34,10 +34,10 @@ function checkOS() {
 	# Check OS version
 	if [[ -e /etc/debian_version ]]; then
 		source /etc/os-release
-		OS="${ID}" # debian or ubuntu
+		OS="${ID}" #debian or ubuntu
 		if [[ ${ID} == "debian" || ${ID} == "raspbian" ]]; then
 			if [[ ${VERSION_ID} -lt 10 ]]; then
-				echo "Your version of Debian (${VERSION_ID}) is not supported. Please use Debian 10 Buster or later"
+				echo "Debian version (${VERSION_ID}) is not supported. Please use Debian 10 or higher..."
 				exit 1
 			fi
 			OS=debian # overwrite if raspbian
@@ -54,7 +54,8 @@ function checkOS() {
 	elif [[ -e /etc/arch-release ]]; then
 		OS=arch
 	else
-		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Oracle or Arch Linux system"
+		echo "Currently, this script only supports Debian, Ubuntu, Fedora, CentOS, Oracle and Arch Linux."
+		echo "Since you are not running a compatible linux version, we will exit to prevent problems."
 		exit 1
 	fi
 }
@@ -68,13 +69,16 @@ function initialCheck() {
 function installQuestions() {
 	#using cloudflare dns to detect external ip because sometimes it is inaccurate using internal data
 	external_ip=`dig +short txt ch whoami.cloudflare @1.0.0.1`
-	#external_ip=`sed -e 's/^"//' -e 's/"$//' <<<"$external_ip"`
-	echo "Welcome to the WireGuard installer!"
-	echo "The git repository is available at: https://github.com/Prakyy/wireguard-install"
+	echo "This script was aquired from: https://github.com/Prakyy/wireguard-install"
 	echo ""
-	echo "I need to ask you a few questions before starting the setup."
-	echo "You can leave the default options and just press enter if you are ok with them."
 	echo ""
+	sleep 1
+	echo "Welcome! Today we will be setting up a new wireguard connection..."
+	sleep 1
+	echo ""
+	echo "You will be asked a few questions before proceeding; The default values are also perfectly acceptable."
+	echo ""
+	sleep 1
 
 	# Detect public IPv4 or IPv6 address and pre-fill for the user
 	SERVER_PUB_IP=`sed -e 's/^"//' -e 's/"$//' <<<"$external_ip"`
@@ -82,12 +86,12 @@ function installQuestions() {
 		# Detect public IPv6 address
 		SERVER_PUB_IP=$(ip -6 addr | sed -ne 's|^.* inet6 \([^/]*\)/.* scope global.*$|\1|p' | head -1)
 	fi
-	read -rp "IPv4 or IPv6 public address: " -e -i "${SERVER_PUB_IP}" SERVER_PUB_IP
+	read -rp "Your external IP address (IPv4/IPv6) is: " -e -i "${SERVER_PUB_IP}" SERVER_PUB_IP
 
 	# Detect public interface and pre-fill for the user
 	SERVER_NIC="$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)"
 	until [[ ${SERVER_PUB_NIC} =~ ^[a-zA-Z0-9_]+$ ]]; do
-		read -rp "Public interface: " -e -i "${SERVER_NIC}" SERVER_PUB_NIC
+		read -rp "Your public interface device is: " -e -i "${SERVER_NIC}" SERVER_PUB_NIC
 	done
 
 	until [[ ${SERVER_WG_NIC} =~ ^[a-zA-Z0-9_]+$ && ${#SERVER_WG_NIC} -lt 16 ]]; do
@@ -95,34 +99,36 @@ function installQuestions() {
 	done
 
 	until [[ ${SERVER_WG_IPV4} =~ ^([0-9]{1,3}\.){3} ]]; do
-		read -rp "Server's WireGuard IPv4: " -e -i 10.66.66.1 SERVER_WG_IPV4
+		read -rp "Your WireGuard-specific IPv4 is: " -e -i 10.66.66.1 SERVER_WG_IPV4
 	done
 
 	until [[ ${SERVER_WG_IPV6} =~ ^([a-f0-9]{1,4}:){3,4}: ]]; do
-		read -rp "Server's WireGuard IPv6: " -e -i fd42:42:42::1 SERVER_WG_IPV6
+		read -rp "Your WireGuard-specific IPv6 is: " -e -i fd42:42:42::1 SERVER_WG_IPV6
 	done
 
 	# Generate random number within private ports range
 	RANDOM_PORT=$(shuf -i49152-65535 -n1)
 	until [[ ${SERVER_PORT} =~ ^[0-9]+$ ]] && [ "${SERVER_PORT}" -ge 1 ] && [ "${SERVER_PORT}" -le 65535 ]; do
-		read -rp "Server's WireGuard port [1-65535]: " -e -i "${RANDOM_PORT}" SERVER_PORT
+		read -rp "This random port will be used for WireGuard [1-65535]: " -e -i "${RANDOM_PORT}" SERVER_PORT
 	done
 
-	# Adguard DNS by default
+	# Cloudflare DNS by default
 	until [[ ${CLIENT_DNS_1} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; do
-		read -rp "First DNS resolver to use for the clients: " -e -i 94.140.14.14 CLIENT_DNS_1
+		read -rp "First DNS resolver: " -e -i 1.1.1.1 CLIENT_DNS_1
 	done
 	until [[ ${CLIENT_DNS_2} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; do
-		read -rp "Second DNS resolver to use for the clients (optional): " -e -i 94.140.15.15 CLIENT_DNS_2
+		read -rp "Second DNS resolver (optional - you may leave it blank): " -e -i 1.0.0.1 CLIENT_DNS_2
 		if [[ ${CLIENT_DNS_2} == "" ]]; then
 			CLIENT_DNS_2="${CLIENT_DNS_1}"
 		fi
 	done
 
 	echo ""
-	echo "Okay, that was all I needed. We are ready to setup your WireGuard server now."
-	echo "You will be able to generate a client at the end of the installation."
-	read -n1 -r -p "Press any key to continue..."
+	echo ""
+	echo "Thanks for confirming"
+	echo ""
+	sleep 0.5
+	read -n1 -r -p "Press any key to proceed with installation..."
 }
 
 function installWireGuard() {
@@ -174,21 +180,21 @@ function installWireGuard() {
 
 	# Save WireGuard settings
 	echo "SERVER_PUB_IP=${SERVER_PUB_IP}
-SERVER_PUB_NIC=${SERVER_PUB_NIC}
-SERVER_WG_NIC=${SERVER_WG_NIC}
-SERVER_WG_IPV4=${SERVER_WG_IPV4}
-SERVER_WG_IPV6=${SERVER_WG_IPV6}
-SERVER_PORT=${SERVER_PORT}
-SERVER_PRIV_KEY=${SERVER_PRIV_KEY}
-SERVER_PUB_KEY=${SERVER_PUB_KEY}
-CLIENT_DNS_1=${CLIENT_DNS_1}
-CLIENT_DNS_2=${CLIENT_DNS_2}" >/etc/wireguard/params
+	SERVER_PUB_NIC=${SERVER_PUB_NIC}
+	SERVER_WG_NIC=${SERVER_WG_NIC}
+	SERVER_WG_IPV4=${SERVER_WG_IPV4}
+	SERVER_WG_IPV6=${SERVER_WG_IPV6}
+	SERVER_PORT=${SERVER_PORT}
+	SERVER_PRIV_KEY=${SERVER_PRIV_KEY}
+	SERVER_PUB_KEY=${SERVER_PUB_KEY}
+	CLIENT_DNS_1=${CLIENT_DNS_1}
+	CLIENT_DNS_2=${CLIENT_DNS_2}" >/etc/wireguard/params
 
 	# Add server interface
 	echo "[Interface]
-Address = ${SERVER_WG_IPV4}/24,${SERVER_WG_IPV6}/64
-ListenPort = ${SERVER_PORT}
-PrivateKey = ${SERVER_PRIV_KEY}" >"/etc/wireguard/${SERVER_WG_NIC}.conf"
+	Address = ${SERVER_WG_IPV4}/24,${SERVER_WG_IPV6}/64
+	ListenPort = ${SERVER_PORT}
+	PrivateKey = ${SERVER_PRIV_KEY}" >"/etc/wireguard/${SERVER_WG_NIC}.conf"
 
 	if pgrep firewalld; then
 		FIREWALLD_IPV4_ADDRESS=$(echo "${SERVER_WG_IPV4}" | cut -d"." -f1-3)".0"
@@ -210,7 +216,10 @@ net.ipv6.conf.all.forwarding = 1" >/etc/sysctl.d/wg.conf
 	systemctl enable "wg-quick@${SERVER_WG_NIC}"
 
 	newClient
-	echo "If you want to add more clients, you simply need to run this script another time!"
+	echo "And we are done..." 
+	echo ""
+	echo "You should run this script again if you need to add new users or remove"
+	echo "old ones (don't worry - we won't be going through the whole setup process again)."
 
 	# Check if WireGuard is running
 	systemctl is-active --quiet "wg-quick@${SERVER_WG_NIC}"
@@ -228,7 +237,7 @@ function newClient() {
 	ENDPOINT="${SERVER_PUB_IP}:${SERVER_PORT}"
 
 	echo ""
-	echo "Tell me a name for the client."
+	echo "Specify new user's name"
 	echo "The name must consist of alphanumeric character. It may also include an underscore or a dash and can't exceed 15 chars."
 
 	until [[ ${CLIENT_NAME} =~ ^[a-zA-Z0-9_-]+$ && ${CLIENT_EXISTS} == '0' && ${#CLIENT_NAME} -lt 16 ]]; do
@@ -365,10 +374,9 @@ function revokeClient() {
 
 function uninstallWg() {
 	echo ""
-	read -rp "Do you really want to remove WireGuard? [y/n]: " -e -i n REMOVE
-	if [[ $REMOVE == 'y' ]]; then
+	read -rp "Please confirm removal of WireGuard by typing 'yes' " -e -i n REMOVE
+	if [[ $REMOVE == 'yes' ]]; then
 		checkOS
-
 		systemctl stop "wg-quick@${SERVER_WG_NIC}"
 		systemctl disable "wg-quick@${SERVER_WG_NIC}"
 
@@ -404,31 +412,34 @@ function uninstallWg() {
 		WG_RUNNING=$?
 
 		if [[ ${WG_RUNNING} -eq 0 ]]; then
-			echo "WireGuard failed to uninstall properly."
+			echo "WireGuard could not be uninstalled properly."
 			exit 1
 		else
-			echo "WireGuard uninstalled successfully."
+			echo "WireGuard has been completely removed from your system."
 			exit 0
 		fi
 	else
 		echo ""
-		echo "Removal aborted!"
+		echo "Removal cancelled!"
 	fi
 }
 
 function manageMenu() {
-	echo "Welcome to WireGuard-install!"
-	echo "The git repository is available at: https://github.com/angristan/wireguard-install"
+	
+	LightGreen='\033[1;32m'
+	NC='\033[0m' #No Color
+	
 	echo ""
-	echo "It looks like WireGuard is already installed."
+	echo -e "${LightGreen}WireGuard is up and running...${NC}"
+	echo ""
 	echo ""
 	echo "What do you want to do?"
 	echo "   1) Add a new user"
-	echo "   2) Revoke existing user"
-	echo "   3) Uninstall WireGuard"
-	echo "   4) Exit"
+	echo "   2) Revoke an existing user"
+	echo "   3) Remove WireGuard from your system"
+	echo "   4) Go back"
 	until [[ ${MENU_OPTION} =~ ^[1-4]$ ]]; do
-		read -rp "Select an option [1-4]: " MENU_OPTION
+		read -rp "Choose an option [1-4]: " MENU_OPTION
 	done
 	case "${MENU_OPTION}" in
 	1)
